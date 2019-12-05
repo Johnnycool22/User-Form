39842e6104815d51294ca50f8f654c930e64060c
John <jkforsythe@hotmail.com>
2019-12-05 16:02:03 +0000

    validation for phone number created

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ function validateName() {
 
 function validatePostCode() {
   const postcode = document.getElementById('code');
-  const re = /^[A-Z]{2}([0-9]{2})(-[0-9]{1})([A-Z]{2})$/i;
+  const re = /^[a-zA-Z]{2}([0-9]{2})(-[0-9]{1})([a-zA-Z]{2})$/;
 
   if(!re.test(postcode.value)) {
     postcode.classList.add('is-invalid');
@@ -28,15 +28,22 @@ function validatePostCode() {
 
 function validateEmail() {
   const email = document.getElementById('email');
-  const re = /^[a-zA-Z]{2,10}$/;
+  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
 
-  if(!re.test(name.value)) {
-    name.classList.add('is-invalid');
+  if(!re.test(email.value)) {
+    email.classList.add('is-invalid');
   } else {
-    name.classList.remove('is-invalid');
+    email.classList.remove('is-invalid');
   }
 }
 
 function validatePhone() {
+  const phone = document.getElementById('phone');
+  const re = /^\(?\d{11}\)?[-. ]?$/;
 
+  if(!re.test(phone.value)) {
+    phone.classList.add('is-invalid');
+  } else {
+    phone.classList.remove('is-invalid');
+  }
 }
